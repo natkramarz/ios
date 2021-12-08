65dfc7d9d5387014e321637f88ba0b8b3847c7c3
Tomasz Los <tomasz.los@ocado.com>
2021-12-08 12:30:35 +0100

    DDD-506: add field to result json

--- formatter.py
+++ formatter.py
@@ -5,12 +5,14 @@ from parse import Table, Column
 
 def __column_as_json(field: Column):
     return {
+        "fullName": field.full_name,
         "description": field.description if field.description else ""
     }
 
 
 def __table_as_json(table: Table):
     return {
+        "datasetTable": table.datasetAndName(),
         "description": table.description if table.description else "",
         "fields": {column.full_name: __column_as_json(column) for column in
                    table.columns}
