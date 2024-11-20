98412bbcf926defe5275176e203717f6b8c6a115
Natalia Kramarz <nat.kramarz@gmail.com>
2024-11-21 00:51:21 +0100

    feat: init

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Michał Droś on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
